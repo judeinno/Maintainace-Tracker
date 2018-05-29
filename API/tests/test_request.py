50d9fbd5544449c257e6fbdcd13f61ace62f07cc
judeinno <judeinno@gmail.com>
2018-05-29 21:21:39 +0300

    Modify test_request.py file

--- API/tests/test_request.py
+++ API/tests/test_request.py
@@ -1,16 +1,26 @@
-from unittest
-from run import app
+import unittest
+from API import app
 import json
 
 
 class request(unittest.TestCase):
     def setUp(self):
-        self.tester = app.test_client(self)
+        self.tester = app.app.test_client(self)
 
     def test_successful_request_fetch(self):
-        input_data = dict(category='user', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
-    expected_response_message = 'User {} can view all requests.'.format(input_data['username'])
-    get_response = self.tester.get('api/auth/signup')
+        input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
+        get_response = self.tester.get('/user/requests')    
+    
+        self.assertEqual(get_response.status_code, 201)
+        
+
+    def test_successfully_create_request(self):
+        input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
+        get_response = self.tester.post('/user/requests', content_type='application/json', data=json.dumps(input_data))    
+        
+        self.assertEqual(get_response.status_code, 201)
+        
+
 
 
 if __name__ == '__main__':
