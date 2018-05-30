d05029acdda49b5a8edb46aee5a7fbd122e9db6c
judeinno <judeinno@gmail.com>
2018-05-30 19:51:14 +0300

    Modify test_request.py file

--- API/tests/test_request.py
+++ API/tests/test_request.py
@@ -5,12 +5,15 @@ from flask_testing import TestCase
 
 
 class request(TestCase):
+    """ This represents the api test class for the tdd """
     def create_app(self):
         return app
 
     def setUp(self):
         self.client = app.test_client(self)
 
+
+     """ Test api to fetch all user request """
     def test_successful_request_fetch(self):
         with self.client:
             input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
@@ -20,7 +23,7 @@ class request(TestCase):
             self.assertEquals(reply["message"], "successfully fetched request")
             self.assertEqual(get_response.status_code, 201)
         
-
+    """ Test api to create a user request """
     def test_successfully_create_request(self):
         input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
         get_response = self.client.post('/user/requests', content_type='application/json', data=json.dumps(input_data))    
