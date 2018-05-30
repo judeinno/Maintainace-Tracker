be7b17d7455b615850c312eeaa22ce540c3c67bf
judeinno <judeinno@gmail.com>
2018-05-30 10:17:06 +0300

    Modify test_request file

--- API/tests/test_request.py
+++ API/tests/test_request.py
@@ -1,23 +1,30 @@
 import unittest
-from API import app
+from API.webapp import app
 import json
+from flask_testing import TestCase
 
 
-class request(unittest.TestCase):
+class request(TestCase):
+    def create_app(self):
+        return app
+
     def setUp(self):
-        self.tester = app.app.test_client(self)
+        self.client = app.test_client(self)
 
     def test_successful_request_fetch(self):
-        input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
-        get_response = self.tester.get('/user/requests')    
-    
-        self.assertEqual(get_response.status_code, 201)
+        with self.client:
+            input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
+            get_response = self.client.get('/user/requests')
+            reply = json.loads(get_response.data.decode())
+
+            self.assertEquals(reply["message"], "successfully fetched request")
+            self.assertEqual(get_response.status_code, 201)
         
 
     def test_successfully_create_request(self):
         input_data = dict(category='user', username='username', field='Electricity', subfield='buld', request_type='The bulb blew', requst_status="Not yet approved")
-        get_response = self.tester.post('/user/requests', content_type='application/json', data=json.dumps(input_data))    
-        
+        get_response = self.client.post('/user/requests', content_type='application/json', data=json.dumps(input_data))    
+                    
         self.assertEqual(get_response.status_code, 201)
         
 
